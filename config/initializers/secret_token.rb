68675fb58a3c08feffcad3fa154aaa41c1dbb8cd
Teddy <tku@thoughtworks.com>
2013-08-04 22:15:42 -0400

    added form to home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wifidemo::Application.config.secret_key_base = 'ed0cdaa87b05ed9f1d1f4492d207b096e1cd0687987015d177d06b8d240234714b805e390025d42263961be004ab5217abdd91326e3f18e3c0352813f372b9d1'
