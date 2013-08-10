57a0217c6e5889f48d3ffffe3d8fec56d420417c
Teddy <tku@thoughtworks.com>
2013-08-10 16:50:44 -0400

    added seed data for ads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+    Ad.create(name: 'Skyview', image_url: 'http://www.skyviewparc.com/en/images/home_1.jpg', url: 'http://www.skyviewparc.com/en/index.php')
+    Ad.create(name: 'One Fulton Square', image_url: 'http://www.fandtgroup.com/wp-content/uploads/2013/07/1fs_1.jpg', url: 'http://www.fandtgroup.com/?p=1563')
+    Ad.create(name: 'New World Mall', image_url: 'http://www.newworldmallny.com/media/k2/items/cache/9ecd376e5371efaef9aad9bc9143aed8_M.jpg', url: 'http://www.newworldmallny.com/')
